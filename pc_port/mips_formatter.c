5d43a918e93f132b707493aca51ded25e10796d2
blizz <blizz@blizz127.com>
2026-09-09 18:15:19 -0500

    Leaf func_801599A4: SC02_031 [801599A4,801599E0)
    Callee from burned 8016EE40 (jal 0C056669; STOP ra=8016ee78); 15-word
    JR@-2 leaf MATCH retail ov_SC02_031 (span SHA256 6fca1cd1…). House-style
    word export + matched C (D_8018730C[*arg0]()).
    Wired extract_asm_words + mips_formatter under sc02_0031. Not MAIN10.

--- pc_port/mips_formatter.c
+++ pc_port/mips_formatter.c
@@ -927,6 +927,9 @@ static const uint32_t kOverlaySc02_80180DB0Words[] = {
 static const uint32_t kOverlaySc02_80181224Words[] = {
 #include "80181224_sc02_0031_words.inc"
 };
+static const uint32_t kOverlaySc02_801599A4Words[] = {
+#include "801599a4_sc02_0031_words.inc"
+};
 static const uint32_t kOverlaySc02_8016EE40Words[] = {
 #include "8016ee40_sc02_0031_words.inc"
 };
@@ -7170,6 +7173,9 @@ static int formatter_fetch(const FormatterCpu *cpu, uint32_t *out) {
     else if (g_overlay_sc02_0031_words &&
              cpu->pc >= 0x80181224u && cpu->pc < 0x8018124cu)
         instruction = kOverlaySc02_80181224Words[(cpu->pc - 0x80181224u)/4u];
+    else if (g_overlay_sc02_0031_words &&
+             cpu->pc >= 0x801599a4u && cpu->pc < 0x801599e0u)
+        instruction = kOverlaySc02_801599A4Words[(cpu->pc - 0x801599a4u)/4u];
     else if (g_overlay_sc02_0031_words &&
              cpu->pc >= 0x8016ee40u && cpu->pc < 0x8016ee94u)
         instruction = kOverlaySc02_8016EE40Words[(cpu->pc - 0x8016ee40u)/4u];
